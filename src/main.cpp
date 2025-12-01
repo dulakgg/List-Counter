93349a2ab6c6b11b600ae4eb3a6261e5ca4664cb
Jakub Kettner <140708360+dulakgg@users.noreply.github.com>
2025-12-01 21:58:28 +0100

    initial and final v1.0.0 commit

---
+++ src/main.cpp
@@ -0,0 +1,82 @@
+#include <Geode/Geode.hpp>
+#include <Geode/utils/web.hpp>
+#include <Geode/loader/Event.hpp>
+#include <matjson.hpp>
+using namespace geode::prelude;
+
+#include <Geode/modify/LevelInfoLayer.hpp>
+class $modify(MyLevelInfoLayer, LevelInfoLayer) {
+    struct Fields {
+        EventListener<web::WebTask> m_listener;
+        CCLabelBMFont* m_countLabel = nullptr;
+        int m_count = -1;
+    };
+
+    bool init(GJGameLevel* level, bool challenge) {
+        if (!LevelInfoLayer::init(level, challenge)) {
+            return false;
+        }
+        auto winSize = CCDirector::sharedDirector()->getWinSize();
+        this->m_fields->m_countLabel = CCLabelBMFont::create("Lists: ...", "bigFont.fnt");
+        this->m_fields->m_countLabel->setScale(0.5f);
+        this->m_fields->m_countLabel->setAnchorPoint({0.f, 0.f});
+        this->m_fields->m_countLabel->setPosition({10.f, 10.f});
+        this->addChild(this->m_fields->m_countLabel);
+
+        if (auto leftMenu = this->getChildByID("left-side-menu")) {
+            auto btnSprite = CCSprite::createWithSpriteFrameName("GJ_viewListsBtn_001.png");
+            auto btn = CCMenuItemSpriteExtra::create(btnSprite, this, menu_selector(MyLevelInfoLayer::onListCounterButton));
+            btn->setID("list-counter"_spr);
+            leftMenu->addChild(btn);
+            leftMenu->updateLayout();
+        }
+
+        downloadLevelCount(level);
+        return true;
+    }
+
+    void downloadLevelCount(GJGameLevel* level) {
+        if (!level) return;
+        int levelId = level->m_levelID;
+        auto url = std::string("https://ihaveawebsiteidea.com/lists/levelnumber/") + std::to_string(levelId); // don't hate it's a random domain i have no usage of
+
+        this->m_fields->m_listener.bind([this](web::WebTask::Event* e) {
+            if (auto* res = e->getValue()) {
+                auto body = res->string().unwrapOr("");
+                auto parsed = matjson::parse(body);
+                if (!parsed) {
+                    if (this->m_fields->m_countLabel) this->m_fields->m_countLabel->setString("Lists: N/A");
+                    return;
+                }
+                auto json = parsed.unwrap();
+
+                int count = 0;
+                if (json.isObject()) {
+                    auto countRes = json["count"].as<int>();
+                    count = countRes.unwrapOr(0);
+                }
+                // there is no space on level info layer to fit this label :sob: lmao
+                if (this->m_fields->m_countLabel) {
+                    auto text = std::string("Lists: ") + std::to_string(count);
+                    this->m_fields->m_countLabel->setString(text.c_str());
+                }
+                this->m_fields->m_count = count;
+            } else {
+                if (this->m_fields->m_countLabel) this->m_fields->m_countLabel->setString("Loadding");
+            }
+        });
+        auto req = web::WebRequest();
+        this->m_fields->m_listener.setFilter(req.get(url));
+    }
+
+    void onListCounterButton(CCObject*) {
+        int count = this->m_fields->m_count;
+        std::string msg;
+        if (count < 0) {
+            msg = "Wait a second";
+        } else {
+            msg = std::string("Lists: ") + std::to_string(count);
+        }
+        FLAlertLayer::create("List Counter", msg.c_str(), "OK")->show();
+    }
+};
---
+++ support.md
@@ -0,0 +1,5 @@
+if you want to support me <3 
+https://ko-fi.com/dulakgg
+Any questions?
+contact me on discord:
+dulakgg
